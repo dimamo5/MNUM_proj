e428ea5cc55bb5244248884db9e7bbcc1e1f20fc
Diogo Moura <diogomoura13@gmail.com>
2014-12-18 21:56:48 +0000

    Qc quase pronto

--- Proj_MNUM/constantes.h
+++ Proj_MNUM/constantes.h
@@ -1,4 +1,5 @@
-double Vap = 3920; // mL
-double Cp0 = (100 / 60) / (Vap);  // mg/mL
-double Ket = 0.1155 / 60; 
-double Ke = Ket / Vap;
+const double Vap = 3920; // mL
+const double Cp0 = (100 / 60) / (Vap);  // mg/mL
+const double Ket = 0.1155 / 60;
+const double Ke = Ket / Vap;
+const double horas_geradas=(rand() % 3 + 4); //valor aleatorio das horas
--- Proj_MNUM/main.cpp
+++ Proj_MNUM/main.cpp
@@ -6,62 +6,63 @@
 using namespace std;
 
 double Dose(unsigned int t);
-double metodoEuler(double(*f)(double x, double y), double xi, double xf, double	y, double h);
-double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,double y, double h);
-double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,double y, double h);
+double metodoEuler(double (*f)(double x, double y), double xi, double xf,
+		double y, double h);
+double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,
+		double y, double h);
+double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,
+		double y, double h);
 
+void comp_central();
 double f_comp_central(double t, double cp);
 double f_bicomp_central(double t, double cp);
 
 int main() {
 	srand(1); //sempre que o programa executa gera os mesmos valores
 
-	double h = 0.1;
-
-	cout << "metodoEuler: " << metodoEuler(f_comp_central, 0, 30 * 24 * 60, Cp0, h) << endl;
-	cout << "Metodo Runga kutta 2a: " << metodoRungaKutta2a(f_comp_central, 0, 30 * 24 * 60, Cp0, h) << endl;
-	cout << "Metodo Runga kutta 4a: " << metodoRungaKutta4a(f_comp_central, 0, 30 * 24 * 60, Cp0, h) << endl;
+//	double h = 0.1;
+//
+//	cout << "metodoEuler: " << metodoEuler(f_comp_central, 0, 30 * 24 * 60, Cp0, h) << endl;
+//	cout << "Metodo Runga kutta 2a: " << metodoRungaKutta2a(f_comp_central, 0, 30 * 24 * 60, Cp0, h) << endl;
+//	cout << "Metodo Runga kutta 4a: " << metodoRungaKutta4a(f_comp_central, 0, 30 * 24 * 60, Cp0, h) << endl;
+	comp_central();
 
 	system("Pause");
 }
 
-double f_comp_central(double t, double cp){
+double f_comp_central(double t, double cp) {
 
-	return (Dose(t) - Ke*cp) / Vap;
+	return (Dose(t) - Ke * cp) / Vap;
 }
 
-
 /*
-	 { 100/60 ,  0 << t << 60
-D(t) {
-	 { 400/((rand % 3 + 4)*60) ,  60 < t << 360  -> 6h -> 360 minutos : tempo maximo possivel
-	 t = minutos
-*/
+ { 100/60 ,  0 << t << 60
+ D(t) {
+ { 400/((rand % 3 + 4)*60) ,  60 < t << 360  -> 6h -> 360 minutos : tempo maximo possivel
+ t = minutos
+ */
 
 double Dose(unsigned int t) {
-
 	t = t % (60 * 24); //adaptacao para verificacao diaria
 
 	if (t >= 0 && t <= 60) {   //1 ramo da funcao dt
 
-		return (double)100 / 60;
+		return (double) 100 / 60;
 	}
 
 	else if (t > 60 && t <= 360) { //2 ramo da funcao dt
 
-		return (double)400 / ((rand() % 3 + 4) * 60);   //determinacao de 	uma hora aleatoria entre[4, 6]
-	}
-	else
+		return (double) 400 / (horas_geradas * 60); //determinacao de 	uma hora aleatoria entre[4, 6]
+	} else
 		return 0;
 }
 
 //metodo de euler
-double metodoEuler(double(*f)(double x, double y), double xi, double xf, double	y, double h)
-{
+double metodoEuler(double (*f)(double x, double y), double xi, double xf,
+		double y, double h) {
 	unsigned n = (xf - xi) / h;
 
-	for (size_t i = 0; i <= n; ++i)
-	{
+	for (size_t i = 0; i <= n; ++i) {
 		y += h * f(xi, y);
 		xi += h;
 	}
@@ -69,21 +70,19 @@ double metodoEuler(double(*f)(double x, double y), double xi, double xf, double	
 }
 
 /*
-double metodoEuler_melhorado(double(*f)(double x, double y), double xi, double
-xf, double y, double h)
-{
+ double metodoEuler_melhorado(double(*f)(double x, double y), double xi, double
+ xf, double y, double h)
+ {
 
-return f(xi, y);
-}*/
-
+ return f(xi, y);
+ }*/
 
 //metodo Runga - Kutta 2a ordem
-double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,double y, double h)
-{
+double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,
+		double y, double h) {
 	unsigned n = (xf - xi) / h;
 
-	for (size_t i = 0; i <= n; ++i)
-	{
+	for (size_t i = 0; i <= n; ++i) {
 		y += h * f(xi + h / 2, y + h / 2 * f(xi, y));
 		xi += h;
 	}
@@ -91,20 +90,39 @@ double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,dou
 }
 
 //metodo Runga - Kutta 4a ordem
-double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,double y, double h)
-{
+double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,
+		double y, double h) {
 	unsigned n = (xf - xi) / h;
 	double deltaY1, deltaY2, deltaY3, deltaY4;
 
-	for (size_t i = 0; i <= n; ++i)
-	{
+	for (size_t i = 0; i <= n; ++i) {
 		deltaY1 = h * f(xi, y);
 		deltaY2 = h * f(xi + h / 2, y + deltaY1 / 2);
 		deltaY3 = h * f(xi + h / 2, y + deltaY2 / 2);
 		deltaY4 = h * f(xi + h, y + deltaY3);
 		xi += h;
-		y += (1.0 / 6) * deltaY1 + (1.0 / 3) * deltaY2 + (1.0 / 3) *
-			deltaY3 + (1.0 / 6) * deltaY4;
+		y += (1.0 / 6) * deltaY1 + (1.0 / 3) * deltaY2 + (1.0 / 3) *
+
+		deltaY3 + (1.0 / 6) * deltaY4;
 	}
 	return y;
 }
+
+void comp_central() {
+	double S1, S2, S3, Erro, Qc;
+	double h = 0.1;
+	do {
+		S1 = metodoEuler(f_comp_central, 0, 30 * 24 * 60, Cp0, h);
+		S2 = metodoEuler(f_comp_central, 0, 30 * 24 * 60, Cp0, h / 2);
+		S3 = metodoEuler(f_comp_central, 0, 30 * 24 * 60, Cp0, h / 4);
+		cout << S1 << "\t" << S2 << "\t" << S3 << "\t";
+		Qc = (S2 - S1) / (S3 - S2);
+		cout << "h:" << h;
+		cout << "Qc: " << Qc << endl;
+		h = h / 2;
+	} while ((int) round(Qc) != 2);
+
+	Erro = S2 - S1;
+	cout << "Erro" << Erro;
+}
+
