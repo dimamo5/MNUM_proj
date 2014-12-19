e586a642f4d7dcfd0e63fda091a3d1990dd64ff8
sergiomieic <sergio.mieic@gmail.com>
2014-12-19 01:06:36 +0000

    funcao dos zeros

--- Proj_MNUM/main.cpp
+++ Proj_MNUM/main.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 double Dose(unsigned int t);
-double metodoEuler(double (*f)(double x, double y), double xi, double xf,
-		double y, double h);
-double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,
-		double y, double h);
-double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,
-		double y, double h);
+double metodoEuler(double (*f)(double x, double y), double xi, double xf,double y, double h);
+double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,double y, double h);
+double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,double y, double h);
+double sistemaEqDiferenciais1aOrd(double f(double x), double f2(double y), double x, double xf, double h);
+double metodoCorda(double f(double x), double x, double y);
+double metodoNewton(double f(double x), double diff(double x), double x, double y);
 
 void comp_central();
 double f_comp_central(double t, double cp);
@@ -58,8 +58,7 @@ double Dose(unsigned int t) {
 }
 
 //metodo de euler
-double metodoEuler(double (*f)(double x, double y), double xi, double xf,
-		double y, double h) {
+double metodoEuler(double (*f)(double x, double y), double xi, double xf,double y, double h) {
 	unsigned n = (xf - xi) / h;
 
 	for (size_t i = 0; i <= n; ++i) {
@@ -69,8 +68,7 @@ double metodoEuler(double (*f)(double x, double y), double xi, double xf,
 	return y;
 }
 
-double metodoEuler_melhorado(double (*f)(double x, double y), double xi,
-		double xf, double yi, double h) {
+double metodoEuler_melhorado(double (*f)(double x, double y), double xi,double xf, double yi, double h) {
 
 	/*unsigned n = (xf - xi) / h;
 	 double k1;
@@ -91,8 +89,7 @@ double metodoEuler_melhorado(double (*f)(double x, double y), double xi,
 }
 
 //metodo Runga - Kutta 2a ordem
-double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,
-		double y, double h) {
+double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,double y, double h) {
 	unsigned n = (xf - xi) / h;
 
 	for (size_t i = 0; i <= n; ++i) {
@@ -103,8 +100,7 @@ double metodoRungaKutta2a(double f(double x, double y), double xi, double xf,
 }
 
 //metodo Runga - Kutta 4a ordem
-double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,
-		double y, double h) {
+double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,double y, double h) {
 	unsigned n = (xf - xi) / h;
 	double deltaY1, deltaY2, deltaY3, deltaY4;
 
@@ -119,6 +115,69 @@ double metodoRungaKutta4a(double f(double x, double y), double xi, double xf,
 		deltaY3 + (1.0 / 6) * deltaY4;
 	}
 	return y;
+}
+
+//resolve sistema eq difs
+double sistemaEqDiferenciais1aOrd(double f(double x), double f2(double y), double x, double xf, double h)
+{
+	unsigned n = (xf - x) / h;
+
+	double deltak1, deltak2, deltak3, deltak4, deltay1, deltay2, deltay3, deltay4;
+
+	for (unsigned i = 0; i < n; i++)
+	{
+		deltay1 = h * f(x);
+		deltak1 = h * f2(x);
+
+		deltay2 = h * f(x + h / 2, y + deltay1 / 2, z + deltak1 / 2);
+		deltak2 = h * f2(x + h / 2, y + deltay1 / 2, z + deltak1 / 2);
+
+		deltay3 = h * f(x + h / 2, y + deltay2 / 2, z + deltak2 / 2);
+		deltak3 = h * f2(x + h / 2, y + deltay2 / 2, z + deltak2 / 2);
+
+		deltay4 = h * f(x + h, y + deltay3, z + deltak3);
+		deltak4 = h * f2(x + h, y + deltay3, z + deltak3);
+
+		x += h;
+		y += (1.0 / 6) * deltay1 + (1.0 / 3) * deltay2 + (1.0 / 3) * deltay3 + (1.0 / 6) * deltay4;
+		z += (1.0 / 6) * deltak1 + (1.0 / 3) * deltak2 + (1.0 / 3) * deltak3 + (1.0 / 6) * deltak4;
+	}
+
+	return 0;
+}
+
+//=====================
+
+double metodoCorda(double f(double x), double x, double y){
+
+	double w, xn, yn;
+
+	if (f(x)*f(y) > 0) return -1;
+
+	do {
+		xn = x;
+		yn = y;
+
+		w = (x*f(y) - y*f(x)) / (f(y) - f(x));				if (f(xn)*f(w) < 0){			y = w;		}		else x = w;
+
+
+	} while (abs(xn - x) > 0.0001);
+
+	return x;
+}
+
+double metodoNewton(double f(double x), double diff(double x), double x){
+
+	double xn;
+
+	do {
+		xn = x;
+
+		if (diff(x) == 0) return -1;
+
+		x += f(x) / diff(x);
+
+	} while (abs(xn - x > 0.0001));
 }
 
 void comp_central() {
